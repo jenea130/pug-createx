f8bc375793bb48603dfbc796590007f9ff2a158f
jenea130 <jenea130@mail.ru>
2023-08-14 15:27:46 +0300

    search page itro: finished

--- src/assets/js/src/main.js
+++ src/assets/js/src/main.js
@@ -1,12 +1,9 @@
-import animationTitle from "./modules/animation";
 import headerScroll from "./modules/scroll/headerScroll";
 import studiesClass from "./modules/studies/studiesClass";
 
 window.addEventListener("DOMContentLoaded", function () {
-	console.log("some thing for me");
 
 	const studies = document.querySelector('.studies');
-	console.log(studies, "studies");
 	if (studies) {
 		studiesClass();
 	}
