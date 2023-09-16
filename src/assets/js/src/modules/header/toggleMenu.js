7af96ebaac096d7246f30189a3ca81be680b1fb5
serii <seriiburduja@gmail.com>
2023-09-16 09:40:44 +0300

    upd: mobile menu

--- src/assets/js/src/modules/header/toggleMenu.js
+++ src/assets/js/src/modules/header/toggleMenu.js
@@ -1,10 +1,17 @@
-export default function toggleMenu(){
+export default function toggleMenu() {
     const mainMenu = document.querySelector('#js-main-menu');
     const sandwichWrap = document.querySelector('#js-sandwitch-wrap');
-
+    const menu_links = document.querySelectorAll('.main-menu a');
+    const pathname = window.location.pathname;
+    menu_links.forEach(function (link) {
+        const href = link.getAttribute('href');
+        const link_pathname = '/'+href;
+        if (link_pathname === pathname) {
+            link.classList.add('active');
+        }
+    });
     sandwichWrap.addEventListener('mousedown', function (e) {
         e.preventDefault();
-
         if (mainMenu.classList.contains('fixed')) {
             mainMenu.classList.remove('fixed');
         } else {
